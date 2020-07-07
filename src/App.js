aed9e95aef45b6d3f91be410ae02841ecab5ab50
Ivan Klimenko <FrogonBr@gmail.com>
2020-07-07 09:48:26 +0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header>
+        <div className="container">
+        </div>
+      </Header>
     </div>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  height: 100px;
+  background: #5da5a4;
+  position: relative;
+  overflow: hidden;
+`
+
+const HeaderELement = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: #64babb;
+  
+  height: 270px;
+  width: 600px;
+  border-radius: 135px;
+
+  transform: rotate(-40deg);
+`
 
 export const Header = () => {
   return (
-    <header>
-      
-    </header>
+    <StyledHeader>
+      <div className="container">
+        <HeaderELement style={{top: '-220px', left: '-75px'}} />
+        <HeaderELement style={{top: '-300px', left: '325px'}} />
+        <HeaderELement style={{top: '20px', left: '575px'}} />
+        <HeaderELement style={{top: '20px', left: '975px'}} />
+      </div>
+    </StyledHeader>
   )
 }
